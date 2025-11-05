ba7542efb0cd7c6dfc55170ab6a982317694e2f4
Sebastián Contreras Lugo <124106565+Seb4stian53@users.noreply.github.com>
2025-11-05 10:39:58 -0600

    Arreglo clases

--- database/__init__.py
+++ database/__init__.py
@@ -5,7 +5,7 @@ Paquete database - Maneja toda la conexión y operaciones con la base de datos
 from .conn import db, DatabaseConnection
 from .login import Login
 from .registrar import Registrar
-from .alumno_querys import alumnos_querys
+from .alumno_querys import Alumno_querys
 
 # Esto permite importar directamente desde el paquete database
-__all__ = ['db', 'DatabaseConnection', 'Login', 'Registrar', 'alumnos_querysyy']
+__all__ = ['db', 'DatabaseConnection', 'Login', 'Registrar', 'Alumno_querys']
